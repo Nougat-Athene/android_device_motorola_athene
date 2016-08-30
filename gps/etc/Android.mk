e26acc83783486b5b1c795e39ba87a9aa3917f28
Neil Agarwal <regalstreak@gmail.com>
2016-08-30 19:48:49 +0200

    athene: gps | Remove gps.conf. We use the caf version

--- gps/etc/Android.mk
+++ gps/etc/Android.mk
@@ -25,14 +25,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := gps.conf
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := gps.conf
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := izat.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
